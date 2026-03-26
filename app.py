d88b926fe2fd02c33ef27767f9bdbddf0c017445
Uddhav Pisharody <U.Pisharody@student.tudelft.nl>
2026-03-26 21:40:03 +0100

    changed server name

--- app.py
+++ app.py
@@ -397,4 +397,4 @@ with gr.Blocks(title="VisionRestore Toolkit") as demo:
 
 if __name__ == "__main__":
     ensure_dir(TEMP_DIR)
-    demo.launch(server_name="127.0.0.1", server_port=7860)
+    demo.launch(server_name="0.0.0.0", server_port=7860)
